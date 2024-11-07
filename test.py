40e04c9487d33d9620c328b45b5e85f1d1a1ecb8
Catomrad <91577742+Catomrad@users.noreply.github.com>
2024-11-07 10:27:42 +0700

    change test.py

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ def test_main():
     assert credit.calc_ann_credit(flag=False) == 310587.06, "должно быть 310587.06"
     assert credit.calc_diff_credit(flag=False) == 310500.0, "должно быть 310500.0"
 
+    credit = main.CreditCalculator(3000000, 12, 6)
+    assert credit.calc_ann_credit(flag=False) == 310587.06, "должно быть 310587.06"
+    assert credit.calc_diff_credit(flag=False) == 310500.0, "должно быть 310500.0"
+
 if __name__ == '__main__':
     test_main()
     print("tests passed")
